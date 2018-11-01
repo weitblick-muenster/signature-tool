264bac99b5285cb47b43fb62b234a70fef02877e
Tobias Görtz <tobias.goertz@zweitag.de>
2018-11-02 00:44:47 +0800

    customize to Weitblick signature

--- signature-builder-v02.js
+++ signature-builder-v02.js
@@ -79,70 +79,56 @@ function buildSignature() {
   let name = document.getElementById("name").value
     ? document.getElementById("name").value
     : "Max Mustermann";
-  let phone = document.getElementById("phone").value
-    ? document.getElementById("phone").value
-    : "123456789";
+  let city = document.getElementById("city").value
+    ? document.getElementById("city").value
+    : "Weitblick Münster e.V.";
   let email = document.getElementById("email").value
     ? document.getElementById("email").value
-    : "max.mustermann@yourdomain.de";
-  let pos1 = document.getElementById("position1").value;
-  let pos2 = document.getElementById("position2").value
-    ? document.getElementById("position2").value
-    : "Finanzen";
-  let linkedIn = "";
-  if (document.getElementById("linkedin").value) {
-    let url = document.getElementById("linkedin").value;
-    if ((isMobile || isSocialLinks)) {
-      linkedIn = ` | <a style="color: #0077B5;" href="${url}">linkedin</a>`;
-    } else {
-      linkedIn = `<a href="${url}"><img src="http://yourdomain.de/signaturen-beta/resources/signatur_logo_linkedin.png"></a>`;
-    }
-  }
-  let xing = "";
-  if (document.getElementById("xing").value) {
-    let url = document.getElementById("xing").value;
-    if ((isMobile || isSocialLinks)) {
-      xing = ` | <a style="color: #026466;" href="${url}">xing</a>`;
-    } else {
-      xing = `<a href="${url}"><img src="http://yourdomain.de/signaturen-beta/resources/signatur_logo_xing.png"></a>`;
-    }
+    : "max.mustermann@weitblicker.org";
+  let pos1 = document.getElementById("position1").value
+    ? document.getElementById("position1").value
+    : "";
+  let pos2 = ""
+
+  if (pos1.length > 0){
+    pos2 = "<br>"
   }
   let fb = "";
   if ((isMobile || isSocialLinks)) {
-    fb = `<a style="color: rgb(59, 89, 152);" href="https://www.facebook.com/linktofb/">facebook</a>`;
+    fb = `<a style="color: rgb(59, 89, 152);" href="https://www.facebook.com/WeitblickMuenster/">facebook</a>`;
   } else {
-    fb = `<a href="https://www.facebook.com/linktofb/"><img src="http://yourdomain.de/resources/signatur_logo_facebook.png" alt=""></a>`;
+    fb = `<a href="https://www.facebook.com/WeitblickMuenster/"><img src="resources/signatur_logo_facebook.png" alt=""></a>`;
   }
   let separator = "";
   if (!isMobile) {
     separator =
-      "<br><img src='http://yourdomain.de/signaturen-beta/resources/separator.jpg'>";
+      "<br><img src='resources/separator.jpg'>";
   }
   let logo = "";
   if (!isMobile) {
     logo =
-      "<br><img src='http://yourdomain.de/resources/logo.png' alt='YourName'>";
+      "<br><img style= 'padding: 10px;' src='https://weitblicker.org/sites/default/files/muenster_0.png' alt='YourName'>";
   }
 
   let signatur = `<!DOCTYPE html>
     <html>
     <body>
-        ${separator}
-        ${logo}
-        <p style="font-family: Tahoma, sans-serif; line-height: 110%; font-size: 10pt; margin: 13.333px 0px 13.333px 0px">
-            <span style="font-size: 11pt;">${name}</span>
-            <br>
-            <span style="color: #0e4194;">${pos1} ${pos2}</span>
-            <br> ${email}
-            <br> +49 (0) ${phone}
-            <br>
-            <br> YourName
+      ${logo}
+      <br>
+      <p style="border-top: 1px dotted #FF9900; font-family: 'Open Sans', sans-serif; font-size: 9pt; color: #000000; font-weight: 300; padding-top: 10px; width: 270px">
+          <span style= "font-size: 12pt; color: #FF9900; font-weight: lighter; margin: 0 0 6px 0;">${name}</span>
+          <br>
+          ${pos2} ${pos1}
+          <br> ${email}
+          <br><span style="font-size: 9pt; color: #000000; font-weight: lighter;	margin: 0px;">
+            <br> ${city}
             <br> Universitätsstraße 14-16 | 48143 Münster
-            <br> <a style="color:black;" href="yourdomain.de">yourdomain</a>
-        </p>
-        <div style="font-family: Tahoma, sans-serif; line-height: 110%; font-size: 10pt; margin: 13.333px 0px 13.333px 0px">
-            ${fb}${linkedIn}${xing}
-        </div>
+            <br><a style="text-decoration: none; color: #000000; font-weight: bold;	margin: 0px;" href="http://www.weitblicker.org/Stadt/Münster">www.weitblicker.org/Stadt/Münster</a>
+          </span>
+      </p>
+      <div style="font-family: Tahoma, sans-serif; line-height: 110%; font-size: 10pt; margin: 13.333px 0px 13.333px 0px">
+          ${fb}
+      </div>
     </body>
     </html>`;
   signatur = replaceUmlauts(signatur);
